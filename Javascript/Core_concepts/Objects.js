ae413b21f8b91726ba0d48f6a2d4fd1650b53bcf
Kinjal Jain <jainkinjal021196@gmail.com>
2025-01-07 16:15:50 +0530

    looping in objects added

--- Javascript/Core_concepts/Objects.js
+++ Javascript/Core_concepts/Objects.js
@@ -47,3 +47,33 @@ const firstCopy = Object.assign({}, first);
 firstCopy.lName = "Bhul";
 console.log(firstCopy.lname) ; // Bhul;
 
+// -----------------------------------------
+// Looping objects - object keys , values and entities
+
+Object.keys(name_of_Object);
+// returns an array with all teh keys of the object
+
+let a = {
+    key1: 123,
+    key2: 345,
+    key3: 576
+}
+for (let i of Object.keys(a))
+    console.log(i);         // key1 key2 key3
+
+Object.values(name_of_Object);
+// it gives an array of all values
+for(let i of Object.values(a))
+    console.log(i)         // 123 346 576
+
+Object.entries(name_of_Object);
+// give array of key -value pairs
+
+for (let i of Object.entries(a))
+    console.log(i);
+
+// ['key1' , 123]
+// ['key2', 345]
+// ['key3', 576]
+
+
